af6099db3da41023310e8186d55614d8c06a7686
Jiho Bok <bok.jiho@gmail.com>
2025-11-21 20:01:47 +0000

    chore: create json data

--- src/_data/jihoBok.ts
+++ src/_data/jihoBok.ts
@@ -4,7 +4,10 @@ const basic = {
   name: "Jiho Bok",
   roleTitle: "Frontend Engineer",
   email: "bok.jiho@gmail.com",
-  summary: "",
+  summary:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut posuere odio. Vivamus malesuada fermentum eleifend. Cras laoreet, nulla scelerisque interdum tristique, mi felis hendrerit arcu, nec blandit augue quam nec tortor. Fusce et lobortis lectus. Nulla vel dui id turpis fringilla rutrum sed non nulla. Proin et tempor nunc, non blandit tellus. In eu elit vel orci porttitor aliquet nec a enim. Donec sit amet nisl a ante fringilla hendrerit.",
+  linkedIn: "https://www.linkedin.com/in/jiho-bok/",
+  github: "https://github.com/Jiho31",
 };
 
 const techStack = [
@@ -37,7 +40,7 @@ const experiences: Experience[] = [
     roleType: "full-time",
     startDate: "Apr 2023",
     endDate: "Jun 2025",
-    description: [""],
+    description: ["acheivement #1", "acheivement #2", "acheivement #3"],
     techStack: [
       "JavaScript(ES6+)",
       "Vue.js",
@@ -57,11 +60,46 @@ const experiences: Experience[] = [
     techStack: [
       "TypeScript",
       "React",
-      "Styled-component",
+      "Styled-components",
+      "WebRTC",
+      "kurento-utils",
       "Figma",
-      "GitLab (CI/CD)",
+      "GitLab",
     ],
   },
 ];
 
-export { basic, techStack, experiences };
+const projects = [
+  {
+    name: "Bookit List",
+    repositoryLink: "https://github.com/Jiho31/bookit-list-v2",
+    liveServiceLink: "https://bookit-list-v2.netlify.app/",
+    techStack: [
+      "TypeScript",
+      "React",
+      "Tailwind CSS",
+      "Firestore",
+      "Firebase Authentication",
+    ],
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut posuere odio. Vivamus malesuada fermentum eleifend. Cras laoreet, nulla scelerisque interdum tristique, mi felis hendrerit arcu, nec blandit augue quam nec tortor. ",
+    imageSrcs: ["/temp_image.jpg" /* "/temp_image.jpg" */],
+  },
+  {
+    name: "Bookit List",
+    repositoryLink: "https://github.com/Jiho31/bookit-list-v2",
+    liveServiceLink: "https://bookit-list-v2.netlify.app/",
+    techStack: [
+      "TypeScript",
+      "React",
+      "Tailwind CSS",
+      "Firestore",
+      "Firebase Authentication",
+    ],
+    description:
+      "Fusce et lobortis lectus. Nulla vel dui id turpis fringilla rutrum sed non nulla. Proin et tempor nunc, non blandit tellus. In eu elit vel orci porttitor aliquet nec a enim. Donec sit amet nisl a ante fringilla hendrerit.",
+    imageSrcs: [],
+  },
+];
+
+export { basic, techStack, experiences, projects };
