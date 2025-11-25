573e008441b7a227969360007ce85c34f9375235
Jiho Bok <bok.jiho@gmail.com>
2025-11-25 21:47:54 +0000

    feat: add footer
    + navigation feature: scroll to specific element

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,16 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <footer className="flex flex-col gap-2 sm:flex-row sm:justify-between border-t border-gray-200 w-full p-5 md:max-w-4xl md:mx-auto">
       <ul className="flex">
-        <li>About Me</li>
+        <Link href="#about-me-section">About Me</Link>
         <span className="px-2"> • </span>
-        <li>Experiences</li>
+        <Link href="#experiences-section">Experiences</Link>
         <span className="px-2"> • </span>
-        <li>Projects</li>
-        <span className="px-2"> • </span>
-        <li>Blog</li>
+        <Link href="#projects-section">Projects</Link>
+        {/* <span className="px-2"> • </span>
+        <li>Blog</li> */}
       </ul>
       <span>© Jiho Bok</span>
       {/* <ul>
