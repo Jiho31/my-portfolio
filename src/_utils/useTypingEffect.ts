d528c20d6e9ccd206d247ccef779ccea608d6dad
Jiho Bok <bok.jiho@gmail.com>
2025-11-30 22:22:29 +0000

    feat: add typing effect custom hook and style

---
+++ src/_utils/useTypingEffect.ts
@@ -0,0 +1,61 @@
+import { useCallback, useRef, useState } from "react";
+
+export default function useTypingEffect() {
+  const textArray = ["Jiho", "a Frontend Engineer"];
+  const TYPING_DELAY = 200;
+  const ERASING_DELAY = 100;
+  const NEW_TEXT_DELAY = 2000; // Delay between current and next text
+
+  const [isTyping, setIsTyping] = useState(false);
+  const [typedTextContent, setTypedTextContent] = useState("");
+  const charIndexRef = useRef(0);
+  const textArrayIndexRef = useRef(0);
+
+  const type = useCallback(() => {
+    if (charIndexRef.current < textArray[textArrayIndexRef.current].length) {
+      if (!isTyping) {
+        setIsTyping(true);
+      }
+
+      setTypedTextContent(
+        (prev) =>
+          prev +
+          textArray[textArrayIndexRef.current].at(charIndexRef.current - 1)
+      );
+      charIndexRef.current++;
+      setTimeout(type, TYPING_DELAY);
+    } else {
+      setIsTyping(false);
+      setTimeout(erase, NEW_TEXT_DELAY);
+    }
+  }, [isTyping]);
+
+  const erase = useCallback(() => {
+    if (charIndexRef.current > 0) {
+      if (!isTyping) {
+        setIsTyping(true);
+      }
+      charIndexRef.current--;
+      setTypedTextContent(
+        textArray[textArrayIndexRef.current].slice(0, charIndexRef.current)
+      );
+      setTimeout(erase, ERASING_DELAY);
+    } else {
+      setIsTyping(false);
+      textArrayIndexRef.current =
+        textArrayIndexRef.current + 1 >= textArray.length
+          ? 0
+          : textArrayIndexRef.current + 1;
+      setTypedTextContent("");
+      setTimeout(type, TYPING_DELAY + 1100);
+    }
+  }, [isTyping, type]);
+
+  const initTypingAnimation = useCallback(() => {
+    if (textArray.length) {
+      setTimeout(type, NEW_TEXT_DELAY + 250);
+    }
+  }, [type]);
+
+  return { initTypingAnimation, isTyping, typedTextContent };
+}
